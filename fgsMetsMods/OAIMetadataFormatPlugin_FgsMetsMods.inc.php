387fcbacc88e4b4a12f1de85dcfdfc74316bb9b7
Lars Rosenström <lars.rosenstrom@kb.se>
2021-03-26 12:25:06 +0100

    Bump plugin version

--- fgsMetsMods/OAIMetadataFormatPlugin_FgsMetsMods.inc.php
+++ fgsMetsMods/OAIMetadataFormatPlugin_FgsMetsMods.inc.php
@@ -57,7 +57,7 @@ class OAIMetadataFormatPlugin_FgsMetsMods extends OAIMetadataFormatPlugin {
 	}
 
 	static function getVersion() {
-        return '1.0.0';
+        return '1.0.2';
     }
 
     static function getUrl() {
