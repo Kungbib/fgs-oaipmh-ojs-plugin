4d6a1a4c0a92b0c2f9b64af7e3562c69395165d5
Lars Rosenström <lars.rosenstrom@kb.se>
2023-10-02 11:39:27 +0200

    Bump version to 1.1.7

--- fgsMetsMods/OAIMetadataFormatPlugin_FgsMetsMods.inc.php
+++ fgsMetsMods/OAIMetadataFormatPlugin_FgsMetsMods.inc.php
@@ -3,7 +3,7 @@
 /**
  * @file OAIMetadataFormatPlugin_FgsMetsMods.inc.php
  *
- * Copyright (c) 2022 National Library of Sweden
+ * Copyright (c) 2023 National Library of Sweden
  * Distributed under the GNU GPL v3. For full terms see LICENSE in the plugin repository root.
  *
  * @class OAIMetadataFormatPlugin_FgsMetsMods
@@ -57,7 +57,7 @@ class OAIMetadataFormatPlugin_FgsMetsMods extends OAIMetadataFormatPlugin {
 	}
 
 	static function getVersion() {
-        return '1.1.6';
+        return '1.1.7';
     }
 
     static function getUrl() {
